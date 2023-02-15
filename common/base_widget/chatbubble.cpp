196eac4c9a75aa27cedcc4172c34bbd3b5907176
zhangbin <zhangbin@joynova.com.cn>
2023-02-15 11:04:43 +0800

    气泡支持文件预览框显示

--- common/base_widget/chatbubble.cpp
+++ common/base_widget/chatbubble.cpp
@@ -7,7 +7,7 @@
 ChatBubble::ChatBubble(QWidget *parent, ENUM_BubbleOrient orient, QVector<MessageFileRecap> vecMessageFileRecap) : QWidget(parent)
 {
     m_iMaxWidth = 360;
-    m_iMinWidth = 60;
+    m_iMinWidth = 220;
 //    this->setMinimumSize(m_iMinWidth, 60);
 //    this->setMaximumSize(m_iMaxWidth, 6000);
     m_bubbleOrient = orient;
@@ -89,6 +89,8 @@ void ChatBubble::paintEvent(QPaintEvent *e)
         }
         if (m_vecMessageFileRecap[i].MessageFileType == 3) {
             // 文件...
+            curHeight = 80;
+            curWight = 200;
         }
 
         // 宽度
@@ -175,6 +177,8 @@ void ChatBubble::paintEvent(QPaintEvent *e)
         }
         if (m_vecMessageFileRecap[i].MessageFileType == 3) {
             // 文件...
+            curHeight = 80;
+            curWight = 200;
         }
 
         // 宽度
@@ -202,6 +206,32 @@ void ChatBubble::paintEvent(QPaintEvent *e)
 //                painter.drawText(rect, 0, m_vecMessageFileRecap[i].FileData);//字体居中
             }
 
+            if (m_vecMessageFileRecap[i].MessageFileType == 3) {
+                // 文件预览框
+                QPainter painter_file(this);
+                painter_file.setRenderHint( QPainter::Antialiasing, true );
+                QPen pen_file;
+                pen_file.setColor(QColor(255,255,255));
+                painter_file.setPen(pen_file);
+
+                painter_file.setBrush(QBrush(QColor(255,255,255)));
+                QRectF rectangleBubble(x, y, curWight, curHeight);
+                painter_file.drawRoundedRect(rectangleBubble,15,15);
+
+                QFont font_file("", 13, QFont::Normal, true);
+                painter_file.setPen(QColor(Qt::gray));
+                painter_file.setFont(font_file);
+
+                // 文件缩略图
+                QImage img(":/img/file");
+                img = img.scaled(QSize(50,50), Qt::KeepAspectRatio);
+                painter_file.drawImage(x + 10, y + 10,img);
+
+                painter_file.drawText(QRectF(x + 70, y + 10, 100, 20), 0, m_vecMessageFileRecap[i].FileName);//字体居中
+                painter_file.drawText(QRectF(x + 70, y + 40, 100, 20), 0, QString::number(m_vecMessageFileRecap[i].FileSize));//字体居中
+
+            }
+
             iTempWight += curWight;
         }
     }
@@ -211,84 +241,3 @@ void ChatBubble::paintEvent(QPaintEvent *e)
         emit sig_resize(iBubbeHeight + 40);
     }
 }
-
-//void ChatBubble::paintEvent(QPaintEvent *e)
-//{
-//    QFont font("", 16, QFont::Bold, true);
-
-//    // 获取文字的长度
-//    QString sNewText;
-//    QString sTempText;
-//    int iTextLen = width() - 33;
-
-//    int iHeight = 20;
-//    bool bHanzi = false;
-
-//    for (int i = 0; i < m_sBubbleText.size(); i++)
-//    {
-//        if (m_sBubbleText[i] > 128 || m_sBubbleText[i] < 0){
-//            bHanzi = true;
-//        }
-
-//        if (QFontMetrics(font).width(sTempText + m_sBubbleText[i]) >= iTextLen)
-//        {
-//            sNewText += "\n";
-//            if (bHanzi) {
-//                iHeight += QFontMetrics(font).height() + 5;
-//            }else{
-//                iHeight += QFontMetrics(font).height() - 2;
-//            }
-//            sTempText = "";
-//            bHanzi = false;
-//        }
-//        sNewText += m_sBubbleText[i];
-//        sTempText += m_sBubbleText[i];
-//    }
-
-//    if (iHeight + 40 != height())
-//    {
-////        resize(iBoxLen + 20, iHeight + 40);
-//        qDebug() << "height:" << iHeight + 40;
-//        emit sig_resize(iHeight + 40);
-//    }
-
-//    QRectF rectangle(10, 10, width() - 20, iHeight + 20);
-
-//    QPainter painter(this);
-//    painter.setRenderHint( QPainter::Antialiasing, true );
-//    QPen pen;
-//    pen.setColor(QColor(173,216,67));
-//    painter.setPen(pen);
-
-//    painter.setBrush(QBrush(QColor(173,216,67)));
-//    painter.drawRoundedRect(rectangle,10,10);
-
-//    if (m_bubbleOrient == ENUM_BubbleOrient::ENUM_BubbleLeft) {
-//        QPointF points[3] = {
-//           QPointF(0, 33),
-//           QPointF(11, 28),
-//           QPointF(11, 38),
-//       };
-//       painter.drawPolygon(points, 3);
-//    }
-//    else if (m_bubbleOrient == ENUM_BubbleOrient::ENUM_BubbleRight) {
-//        QPointF points_2[3] = {
-//           QPointF(this->width(), 33),
-//           QPointF(this->width()-11, 28),
-//           QPointF(this->width()-11, 38),
-//       };
-//       painter.drawPolygon(points_2, 3);
-//    }
-
-//    // 绘制文字
-//    painter.setPen(QColor(Qt::black));
-//    painter.setFont(font);
-
-
-//    QRectF rect(20, 16, width(), height());
-//    painter.drawText(rect, 0, sNewText);//字体居中
-
-////    if (iHeight + 40 != height()){
-////        resize(width(), iHeight + 40);
-////    }
-//}
